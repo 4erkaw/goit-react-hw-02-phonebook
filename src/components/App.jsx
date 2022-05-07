0be4c5100903645999359141e3e7f7ce1f02e2d6
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-05-07 14:41:57 +0300

    npm i

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template1
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix';
+import Form from './Form';
+import Contacts from './Contacts';
+import Container from './Container';
+import Filter from './Filter';
+
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = info => {
+    if (this.checkContact(info.name)) {
+      return Notify.failure(`${info.name} is already in contacts list`);
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [{ ...info, id: nanoid() }, ...contacts],
+    }));
+  };
+
+  checkContact = name => {
+    const find = this.state.contacts.find(contact => contact.name === name);
+    return Boolean(find);
+  };
+
+  changeFilter = e => {
+    e.preventDefault();
+    this.setState({
+      filter: e.currentTarget.value,
+    });
+  };
+
+  delete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  filteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const toLowerCaseFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(toLowerCaseFilter)
+    );
+  };
+  render() {
+    const { filter } = this.state;
+    const filteredContacts = this.filteredContacts();
+    return (
+      <>
+        <Container title="Phonebook">
+          <Form addContact={this.addContact} />
+        </Container>
+        <Container title="Contacts">
+          <Filter value={filter} onChange={this.changeFilter} />
+          <Contacts remove={this.delete} contacts={filteredContacts} />
+        </Container>
+      </>
+    );
+  }
+}
